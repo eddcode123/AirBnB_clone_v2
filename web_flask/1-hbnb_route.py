585b5e8d3016e5ccc1db9c4c01d45ffe986fa3e7
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-18 18:53:19 +0300

    script to start flask web app

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""Flask framework"""
+""" script that starts a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_world():
-    """return hello HBNB"""
+def display_message():
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """return HBNB"""
+def hbnb_page():
     return "HBNB"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
