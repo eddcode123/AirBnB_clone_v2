9358bcf8cc6f7b63029944dc422b7ea0622b83da
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-18 19:51:56 +0300

    this commit message is for a script that returns a default string if non is passed as argumnet or the passed string when pages is queried

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,28 +6,25 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_world():
-    """return hello HBNB"""
+def hello_hbnb():
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """return HBNB"""
+def hbnb_page():
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def text(text):
-    """return text given"""
-    return "C {}".format(text.replace("_", " "))
+def c_text(text):
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/', defaults={'text': 'is_cool'})
 @app.route('/python/<text>', strict_slashes=False)
-def display(text):
-    """display “Python ”, followed by the value of the text"""
-    return "Python {}".format(text.replace("_", " "))
+@app.route('/python', strict_slashes=False)
+def python_text(text="is cool"):
+    return f"Python {text.replace('_', ' ')}"
+
 
 
 if __name__ == '__main__':
